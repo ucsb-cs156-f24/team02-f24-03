c0257c061a34e4911a6c0c1ec0d1513846fa3b1b
Chloe <chloeweng@ucsb.edu>
2024-11-04 14:56:14 -0800

    fixture

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,36 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneMenuItem: [
+      {
+        id: 1,
+        diningCommonsCode: "ortega",
+        name: "salad",
+        station: "entrees",
+      },
+    ],
+  
+    threeMenuItems: [
+      {
+        id: 2,
+        diningCommonsCode: "dlg",
+        name: "ice cream",
+        station: "desserts",
+      },
+  
+      {
+        id: 3,
+        diningCommonsCode: "carrillo",
+        name: "fries",
+        station: "sides",
+      },
+  
+      {
+        id: 4,
+        diningCommonsCode: "portola",
+        name: "sandwich",
+        station: "entrees",
+      },
+    ],
+  };
+  
+  export { ucsbDiningCommonsMenuItemFixtures };
+  
