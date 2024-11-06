ff2f9584c8447250455092920be12cf44f6db78f
Sameer Rao <sameerrao@El-MacBook-Air.local>
2024-11-06 15:38:09 -0800

    Create fixtures for MenuItemReview

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,37 @@
+const menuItemReviewFixtures = {
+  oneRestaurant: [
+    {
+      id: 1,
+      itemid: 1,
+      reviewemail: "test@ucsb.edu",
+      stars: 5,
+      comments: "good",
+    },
+  ],
+
+  threeRestaurants: [
+    {
+      id: 1,
+      itemid: 1,
+      reviewemail: "test@ucsb.edu",
+      stars: 5,
+      comments: "good",
+    },
+    {
+      id: 2,
+      itemid: 1,
+      reviewemail: "test@ucsb.edu",
+      stars: 0,
+      comments: "bad",
+    },
+    {
+      id: 3,
+      itemid: 1,
+      reviewemail: "test@ucsb.edu",
+      stars: 2,
+      comments: "ok",
+    },
+  ],
+};
+
+export { menuItemReviewFixtures };
