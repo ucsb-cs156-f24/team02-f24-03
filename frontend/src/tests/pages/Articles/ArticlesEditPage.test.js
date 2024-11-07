ffe341605744685d33aebe26358b8892ebd3ef1d
Christopher Chau <christopherchau@ucsb.edu>
2024-11-06 16:05:17 -0800

    EditPage Articles 60 (#84)
    * Closes #53: Moved all the backend CRUD API calls
    
    * Closes #54
    
    * Ran prettier
    
    * Closes articles 55 and 56
    
    * Ran npm prettier
    
    * Accidentally touched the UCSBDateForm file
    
    * Ran prettier again
    
    * Will need to figure out why articlesFormtest line 77 no passing
    
    * Removed failed test case, will go back later
    
    * Removed failed test case, will go back
    
    * Added test cases for utils
    
    * Ran prettier
    
    * Created pages for Articles in navbar
    
    * Ran prettier
    
    * Ran prettier
    
    * Finished create page for articles
    
    * Index page done
    
    * Finished Edit issue
    
    * Passing stryker tests
    
    * Ran prettier
    
    * Passing tests locally
    
    * Changed create page files even though though they were passing in previous PR
    
    * ran prettier
    
    * Passing stryker tests locally
    
    * Fixed story book
    
    * Ran prettier

--- frontend/src/tests/pages/Articles/ArticlesEditPage.test.js
+++ frontend/src/tests/pages/Articles/ArticlesEditPage.test.js
@@ -1,5 +1,4 @@
-import { render, screen } from "@testing-library/react";
-import ArticlesEditPage from "main/pages/Articles/ArticlesEditPage";
+import { fireEvent, render, waitFor, screen } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 
@@ -7,37 +6,201 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
+import ArticlesEditPage from "main/pages/Articles/ArticlesEditPage";
+
+import mockConsole from "jest-mock-console";
+
+const mockToast = jest.fn();
+jest.mock("react-toastify", () => {
+  const originalModule = jest.requireActual("react-toastify");
+  return {
+    __esModule: true,
+    ...originalModule,
+    toast: (x) => mockToast(x),
+  };
+});
+
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => {
+  const originalModule = jest.requireActual("react-router-dom");
+  return {
+    __esModule: true,
+    ...originalModule,
+    useParams: () => ({
+      id: 17,
+    }),
+    Navigate: (x) => {
+      mockNavigate(x);
+      return null;
+    },
+  };
+});
 
 describe("ArticlesEditPage tests", () => {
-  const axiosMock = new AxiosMockAdapter(axios);
+  describe("when the backend doesn't return data", () => {
+    const axiosMock = new AxiosMockAdapter(axios);
 
-  const setupUserOnly = () => {
-    axiosMock.reset();
-    axiosMock.resetHistory();
-    axiosMock
-      .onGet("/api/currentUser")
-      .reply(200, apiCurrentUserFixtures.userOnly);
-    axiosMock
-      .onGet("/api/systemInfo")
-      .reply(200, systemInfoFixtures.showingNeither);
-  };
+    beforeEach(() => {
+      axiosMock.reset();
+      axiosMock.resetHistory();
+      axiosMock
+        .onGet("/api/currentUser")
+        .reply(200, apiCurrentUserFixtures.userOnly);
+      axiosMock
+        .onGet("/api/systemInfo")
+        .reply(200, systemInfoFixtures.showingNeither);
+      axiosMock.onGet("/api/articles", { params: { id: 17 } }).timeout();
+    });
 
-  const queryClient = new QueryClient();
-  test("Renders expected content", async () => {
-    // arrange
+    const queryClient = new QueryClient();
+    test("renders header but table is not present", async () => {
+      const restoreConsole = mockConsole();
 
-    setupUserOnly();
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <ArticlesEditPage />
+          </MemoryRouter>
+        </QueryClientProvider>,
+      );
+      await screen.findByText("Edit Article");
+      expect(
+        screen.queryByTestId("ArticlesForm-title"),
+      ).not.toBeInTheDocument();
+      restoreConsole();
+    });
+  });
 
-    // act
-    render(
-      <QueryClientProvider client={queryClient}>
-        <MemoryRouter>
-          <ArticlesEditPage />
-        </MemoryRouter>
-      </QueryClientProvider>,
-    );
+  describe("tests where backend is working normally", () => {
+    const axiosMock = new AxiosMockAdapter(axios);
 
-    // assert
-    await screen.findByText("Edit page not yet implemented");
+    beforeEach(() => {
+      axiosMock.reset();
+      axiosMock.resetHistory();
+      axiosMock
+        .onGet("/api/currentUser")
+        .reply(200, apiCurrentUserFixtures.userOnly);
+      axiosMock
+        .onGet("/api/systemInfo")
+        .reply(200, systemInfoFixtures.showingNeither);
+      axiosMock.onGet("/api/articles", { params: { id: 17 } }).reply(200, {
+        id: 17,
+        title: "Article17",
+        url: "https:url17.com",
+        explanation: "Explanation17",
+        email: "email17@gmail.com",
+        dateadded: "2026-01-02T00:00",
+      });
+      axiosMock.onPut("/api/articles").reply(200, {
+        id: 17,
+        title: "Article17",
+        url: "https:url17.com",
+        explanation: "Explanation17",
+        email: "email17@gmail.com",
+        dateadded: "2026-01-02T00:00",
+      });
+    });
+
+    const queryClient = new QueryClient();
+    test("renders without crashing", async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <ArticlesEditPage />
+          </MemoryRouter>
+        </QueryClientProvider>,
+      );
+
+      await screen.findByTestId("ArticlesForm-title");
+    });
+
+    test("Is populated with the data provided", async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <ArticlesEditPage />
+          </MemoryRouter>
+        </QueryClientProvider>,
+      );
+
+      await screen.findByTestId("ArticlesForm-title");
+
+      const idField = screen.getByTestId("ArticlesForm-id");
+      const titleField = screen.getByTestId("ArticlesForm-title");
+      const urlField = screen.getByTestId("ArticlesForm-url");
+      const explanationField = screen.getByTestId("ArticlesForm-explanation");
+      const emailField = screen.getByTestId("ArticlesForm-email");
+      const dateaddedField = screen.getByTestId("ArticlesForm-dateadded");
+      const submitButton = screen.getByTestId("ArticlesForm-submit");
+
+      expect(idField).toHaveValue("17");
+      expect(titleField).toHaveValue("Article17");
+      expect(urlField).toHaveValue("https:url17.com");
+      expect(explanationField).toHaveValue("Explanation17");
+      expect(emailField).toHaveValue("email17@gmail.com");
+      expect(dateaddedField).toHaveValue("2026-01-02T00:00");
+      expect(submitButton).toHaveTextContent("Update");
+      expect(submitButton).toBeInTheDocument();
+    });
+
+    test("Changes when you click Update", async () => {
+      render(
+        <QueryClientProvider client={queryClient}>
+          <MemoryRouter>
+            <ArticlesEditPage />
+          </MemoryRouter>
+        </QueryClientProvider>,
+      );
+      await screen.findByTestId("ArticlesForm-title");
+
+      const idField = screen.getByTestId("ArticlesForm-id");
+      const titleField = screen.getByTestId("ArticlesForm-title");
+      const urlField = screen.getByTestId("ArticlesForm-url");
+      const explanationField = screen.getByTestId("ArticlesForm-explanation");
+      const emailField = screen.getByTestId("ArticlesForm-email");
+      const dateaddedField = screen.getByTestId("ArticlesForm-dateadded");
+      const submitButton = screen.getByTestId("ArticlesForm-submit");
+
+      expect(idField).toHaveValue("17");
+      expect(titleField).toHaveValue("Article17");
+      expect(urlField).toHaveValue("https:url17.com");
+      expect(explanationField).toHaveValue("Explanation17");
+      expect(emailField).toHaveValue("email17@gmail.com");
+      expect(dateaddedField).toHaveValue("2026-01-02T00:00");
+      expect(submitButton).toBeInTheDocument();
+
+      fireEvent.change(titleField, { target: { value: "Article17" } });
+      fireEvent.change(urlField, {
+        target: { value: "https:url17.com" },
+      });
+      fireEvent.change(explanationField, {
+        target: { value: "Explanation17" },
+      });
+      fireEvent.change(emailField, { target: { value: "email17@gmail.com" } });
+      fireEvent.change(dateaddedField, {
+        target: { value: "2026-01-02T00:00" },
+      });
+
+      fireEvent.click(submitButton);
+
+      await waitFor(() => expect(mockToast).toBeCalled());
+      expect(mockToast).toBeCalledWith(
+        "Article Updated - id: 17 title: Article17",
+      );
+      expect(mockNavigate).toBeCalledWith({ to: "/articles" });
+
+      expect(axiosMock.history.put.length).toBe(1); // times called
+      expect(axiosMock.history.put[0].params).toEqual({ id: 17 });
+      expect(axiosMock.history.put[0].data).toBe(
+        JSON.stringify({
+          id: 17,
+          title: "Article17",
+          url: "https:url17.com",
+          explanation: "Explanation17",
+          email: "email17@gmail.com",
+          dateadded: "2026-01-02T00:00",
+        }),
+      ); // posted object
+    });
   });
 });
