04dd5a4bb4561c599359370140c9a808b2d801f5
Bharat Saiju <bsaiju@ucsb.edu>
2024-11-10 12:33:57 -0800

    prettier frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,6 @@ import MenuItemReviewIndexPage from "main/pages/MenuItemReview/MenuItemReviewInd
 import MenuItemReviewCreatePage from "main/pages/MenuItemReview/MenuItemReviewCreatePage";
 import MenuItemReviewEditPage from "main/pages/MenuItemReview/MenuItemReviewEditPage";
 
-
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
 import "bootstrap/dist/css/bootstrap.css";
