4c80a5edb4e6681f13a4040192a356f2224fe32e
bsaiju <115181045+bsaiju@users.noreply.github.com>
2024-11-11 00:37:28 -0800

    Update App.js (readded Placeholder)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -135,6 +135,29 @@ function App() {
           <>
             <Route
               exact
+              path="/placeholder"
+              element={<PlaceholderIndexPage />}
+            />
+          </>
+        )}
+        {hasRole(currentUser, "ROLE_ADMIN") && (
+          <>
+            <Route
+              exact
+              path="/placeholder/edit/:id"
+              element={<PlaceholderEditPage />}
+            />
+            <Route
+              exact
+              path="/placeholder/create"
+              element={<PlaceholderCreatePage />}
+            />
+          </>
+        )}
+        {hasRole(currentUser, "ROLE_USER") && (
+          <>
+            <Route
+              exact
               path="/recommendationrequest"
               element={<RecommendationRequestIndexPage />}
             />
