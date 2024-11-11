b9f53dfb2d829a5f7229f0fc20de65eb52df55a0
Bharat Saiju <bsaiju@ucsb.edu>
2024-11-10 23:40:31 -0800

    pretty pt 2 electric boogaloo

--- frontend/src/main/pages/HelpRequest/HelpRequestEditPage.js
+++ frontend/src/main/pages/HelpRequest/HelpRequestEditPage.js
@@ -42,7 +42,9 @@ export default function HelpRequestEditPage({ storybook = false }) {
   });
 
   const onSuccess = (helpRequest) => {
-    toast(`HelpRequest Updated - id: ${helpRequest.id} requester email: ${helpRequest.requesterEmail}`);
+    toast(
+      `HelpRequest Updated - id: ${helpRequest.id} requester email: ${helpRequest.requesterEmail}`,
+    );
   };
 
   const mutation = useBackendMutation(
